f4df704a8fad0247b71a92d97e3d1ad254d9761a
Aye Chan <ayechan.sant@gmail.com>
2023-03-26 12:32:13 -0700

    making skeleton

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 const content = document.getElementById ('content')
 
-class skeleton {
+let skeleton = {
+    head: function makeHead() {
+        const titleContainer = document.createElement('div')
+        const title = document.createElement('h1')
+        //
+        titleContainer.className = 'title-container'
+        //
+        title.innerHTML = 'Two-Do List'
+        //
+        titleContainer.appendChild(title)
+        content.appendChild(titleContainer)
+    },
+    body: function makeBody() {
+        const dailyBody = document.createElement('div')
+        const projectBody = document.createElement('div')
+        //
+        dailyBody.className = 'daily body'
+        projectBody.className  = 'project body'
+        //
+        content.appendChild(dailyBody)
+        content.appendChild(projectBody)
+    },
+    foot: function makeFoot() {
+        const foot = document.createElement('div')
+        const byLine = document.createElement('div')
+        // 
+        foot.className = 'foot'
+        //
+        byLine.innerHTML = 'Created by Aye Chan'
+        //
+        foot.appendChild(byLine)
+        content.appendChild(foot)
+    }
+
+}
+
+let generateHTML = {
     create(page) {
-        if (content == null) {
-            //make home page
+        if (content.innerHTML == false || page === 'home') {
+            skeleton.head()
+            skeleton.body()
+            skeleton.foot()
+
         }
     }
 }
+
+generateHTML.create()
