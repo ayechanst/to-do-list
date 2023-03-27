494d8407205f4d35e76fd8958c1d5feaf830eb67
Aye Chan <ayechan.sant@gmail.com>
2023-03-27 11:04:12 -0700

    to do item class created

--- src/toDoItem.js
+++ src/toDoItem.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+export class ToDoItem {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+  makeToDoItem() {
+    // make item
+  }
+  checkOffToDoItem() {
+    // check off
+  }
+  modifyToDoItem() {
+    // mod item
+  }
+  deleteToDoItem() {
+    // delete item
+  }
+}
