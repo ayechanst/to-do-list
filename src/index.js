d3f82fa04d336d488d70973a7f13c38d9c895d02
Aye Chan <ayechan.sant@gmail.com>
2023-03-27 10:03:20 -0700

    projects.js is properly exported

--- src/index.js
+++ src/index.js
@@ -1,49 +1,55 @@
-import { project } from 'projects.js'
-const content = document.getElementById ('content')
+import { ToDoListContainer } from './projects';
 
-export let skeleton = {
-    head: function makeHead() {
-        const titleContainer = document.createElement('div')
-        const title = document.createElement('h1')
-        //
-        titleContainer.className = 'title-container'
-        //
-        title.innerHTML = 'Two-Do List'
-        //
-        titleContainer.appendChild(title)
-        content.appendChild(titleContainer)
-    },
-    body: function makeBody() {
-        const body = document.createElement('div')
-        const dailyBody = document.createElement('div')
-        const navBar = document.createElement('div')
-        const projectBody = document.createElement('div')
-        //
-        body.className = 'body'
-        dailyBody.className = 'daily daily-and-project'
-        navBar.className = 'nav-bar'
-        projectBody.className = 'project daily-and-project'
-        //
-        body.appendChild(dailyBody)
-        body.appendChild(navBar)
-        body.appendChild(projectBody)
-        content.appendChild(body)
-    },
-    foot: function makeFoot() {
-        const foot = document.createElement('div')
-        const byLine = document.createElement('div')
-        // 
-        foot.className = 'foot'
-        //
-        byLine.innerHTML = 'Created by Aye Chan'
-        //
-        foot.appendChild(byLine)
-        content.appendChild(foot)
-    }
-
+export class skeleton {
+  head() {
+    const content = document.getElementById('content');
+    const titleContainer = document.createElement('div');
+    const title = document.createElement('h1');
+    //
+    titleContainer.className = 'title-container';
+    //
+    title.innerHTML = 'Two-Do List';
+    //
+    titleContainer.appendChild(title);
+    content.appendChild(titleContainer);
+  }
+  body() {
+    const content = document.getElementById('content');
+    const body = document.createElement('div');
+    const dailyBody = document.createElement('div');
+    const navBar = document.createElement('div');
+    const projectBody = document.createElement('div');
+    //
+    body.className = 'body';
+    dailyBody.className = 'daily daily-and-project';
+    navBar.className = 'nav-bar';
+    projectBody.className = 'project daily-and-project';
+    //
+    body.appendChild(dailyBody);
+    body.appendChild(navBar);
+    body.appendChild(projectBody);
+    content.appendChild(body);
+  }
+  foot() {
+    const content = document.getElementById('content');
+    const foot = document.createElement('div');
+    const byLine = document.createElement('div');
+    //
+    foot.className = 'foot';
+    //
+    byLine.innerHTML = 'Created by Aye Chan';
+    //
+    foot.appendChild(byLine);
+    content.appendChild(foot);
+  }
 }
 
-skeleton.head()
-skeleton.body()
-project.daily()
-skeleton.foot()
+const skeletonHead = new skeleton();
+const skeletonBody = new skeleton();
+const skeletonFoot = new skeleton();
+const daily = new ToDoListContainer();
+
+skeletonHead.head();
+skeletonBody.body();
+daily.daily('daily');
+skeletonFoot.foot();
--- src/projects.js
+++ src/projects.js
@@ -1,18 +1,30 @@
-const daily = document.querySelector('daily')
-
-class ToDoListContainer {
-    constructor(type) {
-        this.type = type
-    }
-    daily() {
-        const title = document.createElement('h3')
-        //
-        title.className = 'section-title'
-        // 
-        title.innerHTML = 'Today'
-        //
-        daily.appendChild(title)        
-    }
+export class ToDoListContainer {
+  constructor(type) {
+    this.type = type;
+  }
+  daily() {
+    const daily = document.querySelector('.daily');
+    const title = document.createElement('h3');
+    //
+    title.className = 'section-title';
+    //
+    title.innerHTML = 'Today';
+    //
+    daily.appendChild(title);
+  }
 }
-
-export let project = new ToDoListContainer('daily')
+// export let ToDoListContainer = {
+//     //
+//     title.className = 'section-title';
+//     //
+//     title.innerHTML = 'Today';
+//     //
+//     if (typeof title === 'undefined') {
+//       console.log('fucked up');
+//     } else {
+//       console.log('not fucked up');
+//     }
+//     console.log(title);
+//     console.log(daily);
+//     daily.appendChild(title);
+//   },
