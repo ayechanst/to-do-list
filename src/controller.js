86d719a4e7c53e4e43a5169b7cc6551e42761984
Aye Chan <ayechan.sant@gmail.com>
2023-03-28 09:03:14 -0700

    reconfig file insides

---
+++ src/controller.js
@@ -0,0 +1,13 @@
+import
+class AskModel {
+  whatTodo() {
+    // here we get from the model what needs to happen
+
+  }
+}
+
+class TellView {
+  viewDoThis() {
+    // here we set view to what needs to be displayed
+  }
+}
---
+++ src/model/skeleton.js
@@ -0,0 +1,43 @@
+export class skeleton {
+  head() {
+    const content = document.getElementById('content');
+    const titleContainer = document.createElement('div');
+    const title = document.createElement('h1');
+    //
+    titleContainer.className = 'title-container';
+    //
+    title.innerHTML = 'Two-Do List';
+    //
+    titleContainer.appendChild(title);
+    content.appendChild(titleContainer);
+  }
+  body() {
+    const content = document.getElementById('content');
+    const body = document.createElement('div');
+    const dailyBody = document.createElement('div');
+    const navBar = document.createElement('div');
+    const projectBody = document.createElement('div');
+    //
+    body.className = 'body';
+    dailyBody.className = 'daily daily-and-project';
+    navBar.className = 'nav-bar';
+    projectBody.className = 'project daily-and-project';
+    //
+    body.appendChild(dailyBody);
+    body.appendChild(navBar);
+    body.appendChild(projectBody);
+    content.appendChild(body);
+  }
+  foot() {
+    const content = document.getElementById('content');
+    const foot = document.createElement('div');
+    const byLine = document.createElement('div');
+    //
+    foot.className = 'foot';
+    //
+    byLine.innerHTML = 'Created by Aye Chan';
+    //
+    foot.appendChild(byLine);
+    content.appendChild(foot);
+  }
+}
