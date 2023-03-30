9f9d5c7c9c4830d4e45413b0e9d4b7ee63b961b8
Aye Chan <ayechan.sant@gmail.com>
2023-03-30 10:12:34 -0700

    wanting to kill myself css and html

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import { Build } from './skeleton';
 
 class Load {
   home() {
-    const head = new Build();
-    const body = new Build();
-    const foot = new Build();
+    const home = new Build();
     const project = new Build();
     const item = new Build();
-    head.head();
+    home.homePage();
     project.projectCard();
     item.toDoItemCard();
-
-    body.body();
-    foot.foot();
-    buttonLogic();
   }
   project() {
     // for each project in project data base, load project name
--- src/skeleton.js
+++ src/skeleton.js
@@ -1,55 +1,14 @@
 export class Build {
-  head() {
-    const content = document.getElementById('content');
-    const titleContainer = document.createElement('div');
-    const title = document.createElement('h1');
-    //
-    titleContainer.className = 'title-container';
-    //
-    title.innerHTML = 'Two-Do List';
-    //
-    titleContainer.appendChild(title);
-    content.appendChild(titleContainer);
-  }
-
-  body() {
-    const content = document.getElementById('content');
-    const body = document.createElement('div');
-    const dailyBody = document.createElement('div');
-    const projectBody = document.createElement('div');
-    //
-    body.className = 'body';
-    dailyBody.className = 'daily daily-and-project';
-    projectBody.className = 'project daily-and-project';
-    //
-    body.appendChild(dailyBody);
-    body.appendChild(projectBody);
-    content.appendChild(body);
-  }
-
-  foot() {
-    const content = document.getElementById('content');
-    const foot = document.createElement('div');
-    const byLine = document.createElement('div');
-    //
-    foot.className = 'foot';
-    //
-    byLine.innerHTML = 'Created by Aye Chan';
-    //
-    foot.appendChild(byLine);
-    content.appendChild(foot);
-  }
-
-  projectCard() {
-    const project = document.createElement('div');
+  projectCard(name) {
+    const projectTitle = document.createElement('div');
     let createProjectBtn = document.createElement('button');
-    const projectBody = document.createElement('div');
+    const projectContainer = document.createElement('section');
     //
-    project.className = 'project';
+    projectTitle.className = 'project-title';
     createProjectBtn.id = 'createProjectBtn';
-    projectBody.className = 'project-body';
+    projectContainer.className = 'project-container';
     //
-    project.innerHTML = 'Complete Icewind Dale';
+    project.innerHTML = `${name}`;
     createProjectBtn.innerHTML = 'Create New Project';
     //
     createProjectBtn.addEventListener('click', () => {
@@ -58,7 +17,7 @@ export class Build {
     });
     project.appendChild(createProjectBtn);
     project.appendChild(projectBody);
-    content.appendChild(project);
+    dailyBody.appendChild(project);
   }
 
   toDoItemCard() {
